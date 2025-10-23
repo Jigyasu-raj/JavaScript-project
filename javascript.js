f8e35550dd35ebcc5b084273575e0788c2b3e1fe
Sandeep kumar <skverma895746@gmail.com>
2025-10-23 11:03:45 +0530

    Add files via upload
    javascript projects

---
+++ javascript.js
@@ -0,0 +1,162 @@
+//How to Declare variables:
+// let x = 5;
+// let y = 6;
+// y = 4;
+// How to Compute values:
+//let z = x + y;
+
+//I am a Comment. I do Nothing 
+
+
+
+// const x = 5;
+// const y = 6;
+// const z = x + y;
+//  console.log(z);
+// String
+// let color = "Yellow";
+// let lastName = "Johnson";
+
+// // Number
+// let length = 16;
+// let weight = 7.5;
+
+// // BigInt
+// let x = 1234567890123456789012345n;
+// let y = BigInt(1234567890123456789012345)
+
+// // Boolean
+// let x = true;
+// let y = false;
+
+// // Object
+// const person = {firstName:"John", lastName:"Doe"};
+
+// // Array object
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// // Date object
+// const date = new Date("2022-03-25");
+
+// // Undefined
+// let x;
+// let y;
+
+// // Null
+// let x = null;
+// let y = null;
+
+// // Symbol
+// const x = Symbol();
+// const y = Symbol();
+
+// let a = 10;
+// console.log(typeof(a));
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+// And more ...
+// let x = 6;
+// let y = 3;
+// let z = (x < 10 && y > 1)
+// const cars = ["BMW", "Volvo", "Saab", "Ford"];
+// let len = cars.length;
+
+// let text = "";
+// for (let i = 0; i < len; i++) {
+//   text += cars[i] + "<br>";
+// }
+// console.log(text);
+// function temperatureConveter(n)
+// {
+//      let cel = 5/9 * (n-32);
+//      return cel;
+// }
+// let result = temperatureConveter(98.9);
+// console.log(result);
+//  let x = (n) =>  cel = 5/9 * (n-32);
+//   result = x(98.89);
+//   console.log(result);
+// // Create an Object
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue"
+// };
+// console.log(person);
+// console.log(person.firstName);
+// // Create an Object
+// const person = {};
+
+// Add Properties
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
+// console.log(person);
+// // Create an Object
+// const person = new Object({
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue"
+// //})
+// const d = new Date();
+// console.log(d);
+// // new Date()
+// new Date(date ,string)
+
+// new Date(year,month)
+// new Date(year,month,day)
+// new Date(year,month,day,hours)
+// new Date(year,month,day,hours,minutes)
+// new Date(year,month,day,hours,minutes,seconds)
+// new Date(year,month,day,hours,minutes,seconds,ms)
+
+// new Date(milliseconds)
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// document.write("hii");
+//    let val = document.querySelector("h1");
+//    val.style.color = "green";
+   
+//    val.innerText = "Welcome to my javaScript page";
+//  val.style.fontFamily = "arial";
+//   let bulb = document.querySelector(".bulb");
+
+
+//   on.addEventListener("click" ,function(){
+//     bulb.style.backgroundColor = "yellow";
+
+//   })
+//    off.addEventListener("click" ,function(){
+
+//     bulb.style.backgroundColor = "black";
+//   })
+  let on = document.querySelector("#on");
+  let body = document.querySelector("body");
+//   let off = document.querySelector("#off");
+  
+//   on.addEventListener("click",function()
+// {        if(on.innerHTML == ON)
+// {      on.innerHTML = "off"
+//       body.style.backgroundColor = "black";
+     
+// }
+// else{
+
+// }     on.innerHTML = "on"
+//       body.style.backgroundColor = "white";
+  
+// })
+//   off.addEventListener("click",function()
+// {
+//    body.style.backgroundColor = "black";
+//    body.style.color = "white";
+// })
+
+
+  
+
